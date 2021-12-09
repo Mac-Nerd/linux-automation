b16c4f29e3c0e43f6c1792baa2f60aaebb076b20
Mangin <charles.mangin@charles.mangin>
2021-12-09 09:21:40 -0500

    fixed shellcheck warnings

--- USB-Inventory/Linux-USB-Inventory.sh
+++ USB-Inventory/Linux-USB-Inventory.sh
@@ -4,10 +4,8 @@
 # List all the current USB devices and output "manufacturer | product | serial number | vendor ID | product ID" for each device. 
 # used for generating inventory reports
 
-MANIFEST="/etc/USBManifest.txt"
-
 # Check your privilege
-if [ $(whoami) != "root" ]; then
+if [ "$(whoami)" != "root" ]; then
     echo "This script must be run with root/sudo privileges."
     exit 1
 fi
@@ -20,22 +18,22 @@ printf 'manufacturer name | product name | serial number | vendor ID | product I
 for devicePath in /sys/bus/usb/devices/*
 do
 
-        if [ -f ${devicePath}/bDeviceClass ]
+        if [ -f "${devicePath}/bDeviceClass" ]
         then
 
 # if the path includes a device, it will have a device class.
 # check for "hub" class == "09" and skip
 
-        deviceClass=`cat ${devicePath}/bDeviceClass`
+        deviceClass=$("cat ${devicePath}/bDeviceClass")
 
-                if [ ${deviceClass} != "09" ]  # I don't want no hubs.
+                if [ "${deviceClass}" != "09" ]  # I don't want no hubs.
                 then
-                    	vendorID=`cat ${devicePath}/idVendor` 	# all compliant devices will have these
-                        productID=`cat ${devicePath}/idProduct`
+                    	vendorID=$("cat ${devicePath}/idVendor") 	# all compliant devices will have these
+                        productID=$("cat ${devicePath}/idProduct")
 
-						[ -f ${devicePath}/manufacturer ] && manufacturer=`cat ${devicePath}/manufacturer`|| manufacturer="-"	# not all will have readable names/serials
-						[ -f ${devicePath}/product ] && product=`cat ${devicePath}/product`|| product="-"
-						[ -f ${devicePath}/serial ] && serial=`cat ${devicePath}/serial` || serial="-"
+						[ -f "${devicePath}/manufacturer" ] && manufacturer=$("cat ${devicePath}/manufacturer")|| manufacturer="-"	# not all will have readable names/serials
+						[ -f "${devicePath}/product" ] && product=$("cat ${devicePath}/product")|| product="-"
+						[ -f "${devicePath}/serial" ] && serial=$("cat ${devicePath}/serial") || serial="-"
 
 						printf '%s | %s | %s | %s | %s \n' "$manufacturer" "$product" "$serial" "$vendorID" "$productID"
 
