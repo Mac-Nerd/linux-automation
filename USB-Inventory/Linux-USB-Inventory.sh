16b73074f5d0dcfe887684d24a17ccdc9ae5260e
Mangin <charles.mangin@charles.mangin>
2021-12-13 11:21:04 -0500

    find instead of ls

--- USB-Inventory/Linux-USB-Inventory.sh
+++ USB-Inventory/Linux-USB-Inventory.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 2 - 08 December, 2021
+# Version 2.2 - 13 December, 2021
 
 # List all the current USB devices and output "manufacturer | product | serial number | vendor ID | product ID" for each device. 
 # used for generating inventory reports
@@ -15,7 +15,7 @@ printf 'manufacturer name | product name | serial number | vendor ID | product I
 
 
 # List all the current USB devices
-for devicePath in /sys/bus/usb/devices/*
+for devicePath in $(find -L /sys/bus/usb/devices/ -maxdepth 2 -name bDeviceClass -exec dirname {} \; )
 do
 
 	if [ -f "${devicePath}/bDeviceClass" ]
@@ -28,12 +28,12 @@ do
 
 	if [ "${deviceClass}" != "09" ]	 # I don't want no hubs.
 	then
-		vendorID=$("cat ${devicePath}/idVendor")	# all compliant devices will have these
-		productID=$("cat ${devicePath}/idProduct")
+		vendorID=$(cat "${devicePath}/idVendor")	# all compliant devices will have these
+		productID=$(cat "${devicePath}/idProduct")
 
-		[ -f "${devicePath}/manufacturer" ] && manufacturer=$("cat ${devicePath}/manufacturer")|| manufacturer="-"	# not all will have readable names/serials
-		[ -f "${devicePath}/product" ] && product=$("cat ${devicePath}/product")|| product="-"
-		[ -f "${devicePath}/serial" ] && serial=$("cat ${devicePath}/serial") || serial="-"
+		[ -f "${devicePath}/manufacturer" ] && manufacturer=$(cat "${devicePath}/manufacturer")|| manufacturer="-"	# not all will have readable names/serials
+		[ -f "${devicePath}/product" ] && product=$(cat "${devicePath}/product")|| product="-"
+		[ -f "${devicePath}/serial" ] && serial=$(cat "${devicePath}/serial") || serial="-"
 
 		printf '%s | %s | %s | %s | %s \n' "$manufacturer" "$product" "$serial" "$vendorID" "$productID"
 
